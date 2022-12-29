b8773d27ee4fc62ef43123f7470829ef70fe964a
anisul islam <anisulislam980340@gmail.com>
2022-12-29 17:01:22 +0600

    setup mognodb in the server side

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 const app = express()
+require('dotenv').config()
 
 
 
 //middleware 
 app.use(cors())
 app.use(express())
+
+
+
+
+//mongodb connect
+console.log(process.env.DB_USER)
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b986vqn.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 
 
 app.get('/', (req,res)=>{
